808338e5e64a9716096b961f9c34b6667c97a6cc
lujianrong <jianrong@datastory.com.cn>
2020-12-02 19:28:00 +0800

    spring security

---
+++ spring-security/src/main/java/com/muggle/star/learn/spring/security/SecurityApplication.java
@@ -0,0 +1,18 @@
+package com.muggle.star.learn.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author lujianrong
+ * @since 2020/12/2 19:06
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
---
+++ spring-security/src/main/java/com/muggle/star/learn/spring/security/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.muggle.star.learn.spring.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lujianrong
+ * @since 2020/12/2 19:12
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+
+}
