311199654411299fd746807b047ad487442e8fc1
lujianrong <jianrong@datastory.com.cn>
2020-12-04 15:36:33 +0800

    rsa 加解密

--- spring-security/src/main/java/com/muggle/star/learn/spring/security/utils/RsaUtils.java
+++ spring-security/src/main/java/com/muggle/star/learn/spring/security/utils/RsaUtils.java
@@ -1,25 +1,67 @@
 package com.muggle.star.learn.spring.security.utils;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.stereotype.Component;
+
 import java.security.*;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.spec.*;
 
 /**
  * RSA 工具类
  * @author lujianrong
  * @since 2020/12/3 19:33
  */
+@Component
 public class RsaUtils {
-
 
     private static final int DEFAULT_KEY_SIZE = 512;
 
-    public static KeyPair getKey(String secret, int keySize) throws NoSuchAlgorithmException {
+    /**
+     * 生成公钥与私钥
+     *
+     * @param secret
+     * @param keySize
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
+    public static KeyPair createRSAKey(String secret, int keySize) {
 
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        SecureRandom secureRandom = new SecureRandom(secret.getBytes());
-        keyPairGenerator.initialize(Math.max(keySize, DEFAULT_KEY_SIZE), secureRandom);
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            SecureRandom secureRandom = new SecureRandom(secret.getBytes());
+            keyPairGenerator.initialize(Math.max(keySize, DEFAULT_KEY_SIZE), secureRandom);
 
-        return keyPairGenerator.genKeyPair();
-
+            return keyPairGenerator.genKeyPair();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    /**
+     * 根据私钥获取 KeyPair 对象
+     *
+     * @param privateKeyStr
+     * @return
+     */
+    public static KeyPair getRSAKey(String privateKeyStr) {
+
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+            // 解析私钥
+            byte[] privateKeyBytes = Base64.decodeBase64(privateKeyStr);
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            RSAPrivateCrtKey privateKey = (RSAPrivateCrtKey) keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+
+            // 获取公钥
+            RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(privateKey.getModulus(), privateKey.getPublicExponent());
+            PublicKey publicKey = keyFactory.generatePublic(rsaPublicKeySpec);
+
+            return new KeyPair(publicKey, privateKey);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- spring-security/src/test/java/com/muggle/star/learn/spring/security/utils/RsaUtilsTest.java
+++ spring-security/src/test/java/com/muggle/star/learn/spring/security/utils/RsaUtilsTest.java
@@ -1,10 +1,15 @@
 package com.muggle.star.learn.spring.security.utils;
 
+import com.muggle.star.learn.spring.security.SecurityApplication;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import sun.misc.BASE64Encoder;
 
+import javax.annotation.Resource;
 import java.security.KeyPair;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * RSA 测试
@@ -12,22 +17,43 @@ import java.security.NoSuchAlgorithmException;
  * @author lujianrong
  * @since 2020/12/3
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SecurityApplication.class)
 public class RsaUtilsTest {
 
-    @Test
-    public void test(){
-        try {
-            KeyPair key = RsaUtils.getKey("Life is endless, struggle is endless!", 10);
-            BASE64Encoder base64Encoder = new BASE64Encoder();
-            System.out.println("=========================");
-            System.out.println(base64Encoder.encode(key.getPublic().getEncoded()));
-            System.out.println("=========================");
-            System.out.println(base64Encoder.encode(key.getPrivate().getEncoded()));
-            System.out.println("=========================");
+    @Value("${jwt.key.rsa.private}")
+    private String rsaPrivateKey;
+    @Value("${jwt.key.rsa.public}")
+    private String rsaPublicKey;
 
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
+    @Resource
+    private RsaUtils rsaUtils;
+
+
+    @Test
+    public void testCreateRSAKey() {
+
+        KeyPair key = RsaUtils.createRSAKey("Life is endless, struggle is endless!", 10);
+        BASE64Encoder base64Encoder = new BASE64Encoder();
+        System.out.println("=========================");
+        System.out.println(base64Encoder.encode(key.getPublic().getEncoded()));
+        System.out.println("=========================");
+        System.out.println(base64Encoder.encode(key.getPrivate().getEncoded()));
+        System.out.println("=========================");
+
+
+    }
+
+    @Test
+    public void testGetRSAKey() {
+        KeyPair keyPair = rsaUtils.getRSAKey(rsaPrivateKey);
+        BASE64Encoder base64Encoder = new BASE64Encoder();
+        System.out.println("=========================");
+        System.out.println(base64Encoder.encode(keyPair.getPublic().getEncoded()));
+        System.out.println("=========================");
+        System.out.println(base64Encoder.encode(keyPair.getPrivate().getEncoded()));
+        System.out.println("=========================");
+
     }
 
 }
