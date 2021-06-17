1ebb50803f47fe3755e8bf8b471dc60eaac82f91
lujianrong <lujianrong@reconova.com>
2021-06-17 11:06:55 +0800

    netty 出站入站注释

--- netty/src/main/java/learn/netty/iobdhandler/MessageDecoderLong.java
+++ netty/src/main/java/learn/netty/iobdhandler/MessageDecoderLong.java
@@ -13,6 +13,15 @@ import java.util.List;
  */
 public class MessageDecoderLong extends ReplayingDecoder<Long> {
 
+    /**
+     *  decode 会根据接收的数据，被调用多次, 直到确定没有新的元素被添加到list, 或者是ByteBuf 没有更多的可读字节为止
+     * 如果list out 不为空，就会将list的内容传递给下一个 ChannelInboundHandler 处理, 该处理器的方法也会被调用多次
+     *
+     * @param ctx
+     * @param in
+     * @param out
+     * @throws Exception
+     */
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
         System.out.println("MessageDecoderLong===decode");
